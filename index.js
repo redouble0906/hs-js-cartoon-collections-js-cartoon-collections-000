13bb26260968a1de690527dfb82cfb09f1ca3c20
Durrell King <durrellking1@gmail.com>
2020-02-28 15:36:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function dwarfRollCall(dwarves) {
-}
+var string = [];
+	dwarves.forEach(function(name, d) {
+		string.push(d+1 +". " + name + " ");
+	});
+	return string.join("");
+  }
 
 function summonCaptainPlanet(planeteerCalls){
+ var calls = [];
+    planeteerCalls.forEach(function(scp) {
+  	scp = scp.toUpperCase() + "!";
+  	calls.push(scp);
+  });
+  return calls;
+  
 }
 
 function longPlaneteerCalls(words) {
+for(var d = 0; d < words.length; d++) {
+		if(words[d].length > 4) {
+			return true;
+		}
+	}
+	return false;
+  
 }
 
-function findTheCheese (foods) {
+function findTheCheese (food) {
+ var cheese = ["camembert", "cheddar", "gouda"];
+	for(var d = 0; d < food.length; d++) {
+		var Itscheese = cheese.indexOf(food[d]);
+		if(Itscheese !== -1) {
+			return food[d];
+		}
+	}
+	return "no cheese!";
+  
 }
